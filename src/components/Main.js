c237997c37d017cd33b2596af9786a68ea356294
github username <github email address>
2019-11-27 13:53:26 -0700

    react-router integrated. Navlinks in navbar, switch in Main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import {
+    Switch,
+    Route
+} from "react-router-dom"
+
 import Home from './Home'
+import Map from './Map'
+import Vaccines from './VaccineList'
 
 import '../stylesheets/components/Main.scss'
 
 export default function Main () {
     return (
-        <Home />
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/map' component={Map} />
+            <Route path='/vaccines' component={Vaccines} />
+        </Switch>
     )
 }
