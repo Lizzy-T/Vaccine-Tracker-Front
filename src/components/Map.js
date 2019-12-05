df078c18061a42d993c9a5ae8ee331bd4c7fb372
github username <github email address>
2019-12-05 11:13:39 -0700

    update tableau vizurl

--- src/components/Map.js
+++ src/components/Map.js
@@ -11,7 +11,7 @@ export default class Map extends Component {
 
     initTableau () {
         const vizUrl =
-            "https://10ay.online.tableau.com/t/lizzytongportfolio/views/DiseaseVaccineWorld/WorldwideDiseaseCases/lizzy.tong@aol.com/abeffb9e-9636-4753-881e-e19a1f3f070d?:display_count=n&:showVizHome=n&:origin=viz_share_link";
+            "https://10ay.online.tableau.com/t/lizzytongportfolio/views/DiseaseVaccineWorld/WordwideDiseaseCases?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link";
  
         const options = {
             hideTabs: true,
